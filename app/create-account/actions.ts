b93e582aa8315ba53cbcb7f68ee1b2ae3b0e7123
dana <handany1102@gmail.com>
2025-04-26 22:46:00 +0900

    Authentication - 1

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,6 +1,10 @@
 "use server";
 import { PASSWORD_MIN_LENGTH, PASSWORD_REGEX, PASSWORD_REGEX_ERROR } from "@/lib/constants";
+import db from "@/lib/db";
 import { z } from "zod";
+import bcrypt from "bcrypt";
+import { redirect } from "next/navigation";
+import getSession from "@/lib/session";
 
 const checkUsername = (username: string) => {
     return !username.includes("potato");
@@ -16,6 +20,32 @@ const checkPasswords = ({
     return password === confirm_password;
 };
 
+const checkUniqueUsername = async(username: string) => {
+	const user = await db.user.findUnique({
+		where: {
+			username,
+		},
+		select: {
+			id: true,
+		},
+	});
+
+	return !Boolean(user);
+};
+
+const checkUniqueEmail = async(email: string) => {
+	const user = await db.user.findUnique({
+		where: {
+			email,
+		},
+		select: {
+			id: true
+		}
+	});
+	
+	return !Boolean(user);
+};
+
 const formSchema = z
     .object({
         username: z
@@ -25,9 +55,11 @@ const formSchema = z
             })
 			.trim()
 			.toLowerCase()	
-			.transform((username) => `🔥 ${username} 🔥`)	//값을 변환	
-            .refine(checkUsername, "No potatoes allowed!"),
-        email: z.string().email().trim().toLowerCase(),
+			//.transform((username) => `🔥 ${username} 🔥`)	//값을 변환	
+            .refine(checkUsername, "No potatoes allowed!")
+			.refine(checkUniqueUsername, "This username is already taken"),
+        email: z.string().email().trim().toLowerCase()
+				.refine(checkUniqueEmail,"There is an account already registered with that email."),
         password: z
             .string()
             .min(PASSWORD_MIN_LENGTH)
@@ -40,7 +72,24 @@ const formSchema = z
     .refine(checkPasswords, {
         message: "Both passwords should be the same!",
         path: ["confirm_password"],
-    });
+    })
+	.superRefine(async({username}, ctx) => {
+		const user = await db.user.findUnique({
+			where: {
+				username
+			},
+			select: {
+				id: true
+			}
+		});
+		if(user){
+			ctx.addIssue({
+				code:'custom',
+				message: "This username is already taken",
+			});
+		}
+
+	});
 
 export async function createAccount(prevState: any, formData: FormData) {
     const data = {
@@ -49,16 +98,33 @@ export async function createAccount(prevState: any, formData: FormData) {
         password: formData.get("password"),
         confirm_password: formData.get("confirm_password"),
     };
-    const result = formSchema.safeParse(data);
-    console.log(result);
+    const result = await formSchema.spa(data);
 
     if (!result.success) {
-        // return result.error.flatten();
         return {
             errors: result.error.flatten(),
             values: data, // 입력값도 함께 반환
         };
     }else{
-		console.log(result.data);
+		// check if username is taken		
+		// check if the email already used		
+		// hash password
+		const hashedPassword = await bcrypt.hash(result.data.password, 12);
+		// save the user to db
+		const user = await db.user.create({
+			data: {
+				username: result.data.username,
+				email: result.data.email,
+				password: hashedPassword,
+			},
+			select: {
+				id: true,
+			},
+		});
+		// log the user in
+		const session = await getSession();
+		session.id = user.id;
+		await session.save();
+		redirect("/profile");
 	}
 }
---
+++ app/profile/page.tsx
@@ -0,0 +1,5 @@
+export default function Profile(){
+	return (
+		<h1>welcome to your prifile!</h1>
+	);
+}
