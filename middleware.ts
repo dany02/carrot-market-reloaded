fce93223db2e5782849a347cc96cd280eed49159
dana <handany1102@gmail.com>
2025-04-27 00:12:38 +0900

    Authentication -3

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,31 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import getSession from "./lib/session";
+
+interface Routes {
+	[key: string]: boolean;
+}
+
+const publicOnlyUrls: Routes = {
+	"/":true,
+	"/login": true,
+	"/sms": true,
+	"/create-account":true,
+};
 
 export async function middleware(request: NextRequest) {
-    // const pathname = request.nextUrl.pathname;
-    // if (pathname === "/") {
-    //     const response = NextResponse.next();
-    //     response.cookies.set("middleware-cookie", "hello!");
-    //     return response;
-    // }
-
-    // if (pathname === "/profile") {
-    //     return NextResponse.redirect(new URL("/", request.url));
-    // }
-	console.log('hello');
+	const session = await getSession();
+	const exists = publicOnlyUrls[request.nextUrl.pathname];
+	if(!session.id){
+		if(!exists){
+			return NextResponse.redirect(new URL("/", request.url));
+		}
+	} else {
+		if(exists){
+			return NextResponse.redirect(new URL("/products", request.url));
+		}
+	}
 }
 
 export const config = {
-    //matcher: ["/profile", "/about/:path*", "/dashboard/:path*"],
-	matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+	matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
 };
