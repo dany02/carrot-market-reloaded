3efc6a5fd2b81f33e5bfe3accbc4f89e31ca7eff
dana <handany1102@gmail.com>
2025-04-30 17:33:55 +0900

    Product Upload

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -1,33 +1,41 @@
-
 import ProductList from "@/components/product-list";
 import db from "@/lib/db";
 import { Prisma } from "@/lib/generated/prisma/client";
+import { PlusIcon } from "@heroicons/react/24/solid";
+import Link from "next/link";
 
-
-async function getInitialProducts(){
-	const products = await db.product.findMany({
-		select: {
-			title:true,
-			price:true,
-			created_at:true,
-			photo:true,
-			id:true,
-		},
-		take:1,
-		orderBy:{
-			created_at: "desc",
-		}
-	});
-	return products;
+async function getInitialProducts() {
+    const products = await db.product.findMany({
+        select: {
+            title: true,
+            price: true,
+            created_at: true,
+            photo: true,
+            id: true,
+        },
+        take: 1,
+        orderBy: {
+            created_at: "desc",
+        },
+    });
+    return products;
 }
 
-export type InitialProducts = Prisma.PromiseReturnType<typeof getInitialProducts>;
+export type InitialProducts = Prisma.PromiseReturnType<
+    typeof getInitialProducts
+>;
 
-export default async function Products(){
-	const initialProducts = await getInitialProducts();
-	return(
-		<div>
-			<ProductList initialProducts={initialProducts}/>
-		</div>
-	);
+export default async function Products() {
+    const initialProducts = await getInitialProducts();
+    return (
+        <div>
+            <ProductList initialProducts={initialProducts} />
+            <Link
+                href="/products/add"
+                className="bg-orange-500 flex items-center justify-center rounded-full size-16 fixed bottom-24 right-8 text-white transition-colors hover:bg-orange-400"
+            >
+                <PlusIcon className="size-10" />
+            </Link>
+        </div>
+    );
 }
